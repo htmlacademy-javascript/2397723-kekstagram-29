9d6a512a068423bfd678fc0669f71a0f2be50328
AdonaiJehosua <adonaijehosua@gmail.com>
2023-07-30 19:40:51 +0500

    делает модуль с функцией

--- js/pictures.js
+++ js/pictures.js
@@ -1,21 +1,20 @@
-import { generatePhotosArray } from './data.js';
+const generatePictures = (array) => {
+  const picturesList = document.querySelector('.pictures');
+  const pictureTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+  const pictureFragment = document.createDocumentFragment();
 
-const picturesList = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('a');
+  array.forEach(({ url, description, likes, comments }) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('img').src = url;
+    pictureElement.querySelector('img').alt = description;
+    pictureFragment.appendChild(pictureElement);
+  });
 
-const usersPictures = generatePhotosArray();
+  picturesList.appendChild(pictureFragment);
+};
 
-const pictureFragment = document.createDocumentFragment();
-
-usersPictures.forEach(({ url, description, likes, comments }) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.querySelector('img').src = url;
-  pictureElement.querySelector('img').alt = description;
-  pictureFragment.appendChild(pictureElement);
-});
-
-picturesList.appendChild(pictureFragment)
+export { generatePictures };
