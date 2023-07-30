a91c0b1b0123be3a112bc775acfec56bb5a62337
AdonaiJehosua <adonaijehosua@gmail.com>
2023-07-30 19:41:23 +0500

    импортирует функцию генерации картинок и вызывает ее со сгенерированным массивом

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 import { generatePhotosArray } from './data.js';
+import { generatePictures } from './pictures.js';
+
+const usersPictures = generatePhotosArray();
+
+generatePictures(usersPictures);
