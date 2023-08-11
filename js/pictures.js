6326fcc3565e6e1a0ef00860a6342a890dc26791
AdonaiJehosua <adonaijehosua@gmail.com>
2023-08-11 19:42:05 +0500

    Убирает деструктуризацию объекта, добавляет обработчик событий на элемент списка картинок

--- js/pictures.js
+++ js/pictures.js
@@ -1,3 +1,5 @@
+import { onPictureClick } from './bigPictures.js';
+
 const generatePictures = (array) => {
   const picturesList = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture')
@@ -5,16 +7,19 @@ const generatePictures = (array) => {
     .querySelector('.picture');
   const pictureFragment = document.createDocumentFragment();
 
-  array.forEach(({ url, description, likes, comments }) => {
+  array.forEach((element) => {
     const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    pictureElement.querySelector('img').src = url;
-    pictureElement.querySelector('img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = element.likes;
+    pictureElement.querySelector('.picture__comments').textContent = element.comments.length;
+    pictureElement.querySelector('img').src = element.url;
+    pictureElement.querySelector('img').alt = element.description;
     pictureFragment.appendChild(pictureElement);
+
+    pictureElement.addEventListener('click', onPictureClick(element));
   });
 
   picturesList.appendChild(pictureFragment);
 };
 
 export { generatePictures };
+
