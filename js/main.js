64b8b218cbd7557ea154122c8fb91440153efde2
AdonaiJehosua <adonaijehosua@gmail.com>
2023-10-15 18:41:55 +0500

    Фикс

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 import { initUploadForm } from './upload-form.js';
 import { getPictures } from './api.js';
 import { filterPicturesList } from './filter-pictures.js';
+import { showErrorNotification } from './notifications.js';
+import { FETCH_ERROR_MESSAGE } from './pictures.js';
 
-getPictures(
-  filterPicturesList,
-  showAlert,
-  'Не удалось загрузить фотографии пользователей',
-);
+
+getPictures({
+  onSuccess: filterPicturesList,
+  onError: showErrorNotification,
+  errorMessage: FETCH_ERROR_MESSAGE,
+});
 
 initUploadForm();
