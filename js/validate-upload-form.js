38c2321c89c3246ebc1b803403c4fa2a7a430e6a
AdonaiJehosua <adonaijehosua@gmail.com>
2023-10-24 22:19:42 +0500

    Выносит длину комментария в константу. Упрощает функцию

--- js/validate-upload-form.js
+++ js/validate-upload-form.js
@@ -4,12 +4,13 @@ const imgUploadForm = document.getElementById('upload-select-image');
 
 const HASHTAGS_AMOUNT = 5;
 const HASHTAG_REG = /^#[a-zа-яё0-9]{1,19}$/i;
+const COMMENT_MAX_LENGTH = 140;
 
 const validationErrors = {
   pattern: 'Хэштег не соответствует шаблону',
   amount: 'Максимум 5 хэштегов',
-  dublicate: 'Хэштеги не могут повторяться',
-  maxLength: 'Не больше 140 символов'
+  duplicate: 'Хэштеги не могут повторяться',
+  maxLength: `Не больше ${COMMENT_MAX_LENGTH} символов`
 };
 
 const pristine = new Pristine(imgUploadForm, {
@@ -32,8 +33,8 @@ const checkHashtag = (hashtag) => {
     if (!HASHTAG_REG.test(hashtagItem)) {
       regTest = false;
     }
-    const dublicateHashtags = splitHashtags.filter((el) => el === hashtagItem);
-    if (dublicateHashtags.length > 1) {
+    const duplicateHashtags = splitHashtags.filter((el) => el === hashtagItem);
+    if (duplicateHashtags.length > 1) {
       noDuplicates = false;
     }
   }
@@ -56,13 +57,9 @@ pristine.addValidator(textHashtag, (value) => {
 pristine.addValidator(textHashtag, (value) => {
   const test = checkHashtag(value);
   return test.noDuplicates;
-}, validationErrors.dublicate, 3, true);
+}, validationErrors.duplicate, 3, true);
 
-pristine.addValidator(textDescription, (value) => {
-  if (value.length < 140) {
-    return true;
-  }
-  return false;
-}, validationErrors.maxLength, 1, true);
+pristine.addValidator(textDescription, (value) => value.length <= COMMENT_MAX_LENGTH,
+  validationErrors.maxLength, 1, true);
 
 export { pristine };
